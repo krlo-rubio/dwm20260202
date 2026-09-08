948a2294f08282cb60b6db62a8a9aedb1ae794cc
Carlo Rubio G <carlo.rubio1106@gmail.com>
2026-09-07 23:51:29 -0300

    Update script.js

--- Semana 4/script.js
+++ Semana 4/script.js
@@ -1,1 +1,44 @@
+const queryMenu = `
+  query ObtenerMenuLaChilena {
+    empanadas {
+      id
+      sabor
+      tipoCoccion
+      precio
+      disponible
+    }
+  }
+`;
 
+const queryCliente = `
+  query ObtenerDatosCliente($run: String!) {
+    cliente(run: $run) {
+      run
+      nombreCompleto
+      direccion
+      comuna
+      correoElectronico
+      numeroTelefonico
+    }
+  }
+`;
+
+const queryReporteVentas = `
+  query ReporteVentas {
+    ventasWeb {
+      idPedido
+      fecha
+      total
+      estadoPago
+    }
+  }
+`;
+
+const formulario = document.getElementById('form-registro');
+
+if (formulario) {
+    formulario.addEventListener('submit', function(event) {
+        event.preventDefault();
+        console.log("Validando correo mediante API externa...");
+    });
+}
